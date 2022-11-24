0e96e6a78459e8f487c3d1107081cf97812360f2
KarenHarutyunyan <karenvardanovich@gmail.com>
2022-11-24 23:08:40 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = 'a';   
+            char b = 'b';
+            Console.WriteLine(a+b); //195
+            Console.WriteLine(a.ToString()+b.ToString()); // ab
+
+            Console.ReadKey();  
+        }
+    }
+}
